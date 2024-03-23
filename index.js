394bf23c7b8067d685a1e59df86c96aa56951df0
vundi4773 <vundi4773@gmail.com>
2024-03-23 22:55:23 +0300

    Functions Parameters lab

---
+++ index.js
@@ -0,0 +1,24 @@
+// Describing functions with parameters
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+console.log(introduction("Aki"));
+console.log(introduction("Samip"));
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(introductionWithLanguage("Aki", "Ember.js"));
+console.log(introductionWithLanguage("Samip", "React"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Gracie"));
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+console.log(introductionWithLanguageOptional("Gracie", "Python")); 
